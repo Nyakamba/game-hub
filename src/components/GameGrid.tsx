c4845e99472224b846dc72577f3b60e9940515fb
Nyakamba <omwegaenock@gmail.com>
2024-09-15 11:37:55 +0300

    build for production

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,7 +14,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  if (error) return error && <Text>{error}</Text>;
+  if (error) return <Text>{error}</Text>;
 
   return (
     <SimpleGrid
