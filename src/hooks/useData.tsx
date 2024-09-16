7213db73b5ffeb7e9485e2d3cef276c6913394fc
Nyakamba <omwegaenock@gmail.com>
2024-09-16 08:21:44 +0300

    fetching genres

--- src/hooks/useData.tsx
+++ src/hooks/useData.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { AxiosRequestConfig, CanceledError } from "axios";
 
-interface FetcheResponse<T> {
+export interface FetcheResponse<T> {
   count: number;
   results: T[];
 }
